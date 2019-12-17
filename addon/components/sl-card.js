2866ed6429c454f5b1944f36f0b810f2081bb19f
Jorge Hernandez <jdaviderb@gmail.com>
2019-12-16 23:19:01 -0500

    get started

---
+++ addon/components/sl-card.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: [
+    'playlist h-56 w-56 flex flex-col mt-10 m-5 shadow-2xl relative hover:opacity-75'
+  ]
+});
